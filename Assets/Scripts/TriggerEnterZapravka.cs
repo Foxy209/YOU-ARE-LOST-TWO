552838005b1688d02e6e0447ea5125eefcecaaca
M-Unity <mihailsheshukov10@gmail.com>
2024-06-27 16:32:35 +0500

    Audio update 2

--- Assets/Scripts/TriggerEnterZapravka.cs
+++ Assets/Scripts/TriggerEnterZapravka.cs
@@ -5,6 +5,9 @@ public class TriggerEnterZapravka : MonoBehaviour
     [SerializeField] private GameObject trigg;
     [SerializeField] private Transform spawn;
     [SerializeField] private Animator zatemn;
+    [SerializeField] private bool DoYouNeedToChangeClip;
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private AudioSource sour;
     private void Awake() => zatemn.StopPlayback();
     private void OnTriggerEnter(Collider other)
     {
@@ -18,6 +21,11 @@ public class TriggerEnterZapravka : MonoBehaviour
     {
         yield return new WaitForSeconds(1);
         plr.GetComponent<CharacterController>().enabled = false;
+        if (DoYouNeedToChangeClip)
+        {
+            sour.clip = clip;
+            sour.Play();
+        }
         plr.transform.position = spawn.position;
         plr.GetComponent<CharacterController>().enabled = true;
         Destroy(trigg);
