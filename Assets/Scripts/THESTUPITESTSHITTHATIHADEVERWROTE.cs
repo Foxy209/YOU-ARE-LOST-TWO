a2a1e23de4003a21304ad40c74e54e2a875ebb67
M-Unity <mihailsheshukov10@gmail.com>
2025-06-19 05:18:51 +0500

    Update THESTUPITESTSHITTHATIHADEVERWROTE.cs

--- Assets/Scripts/THESTUPITESTSHITTHATIHADEVERWROTE.cs
+++ Assets/Scripts/THESTUPITESTSHITTHATIHADEVERWROTE.cs
@@ -7,12 +7,6 @@ public class THESTUPITESTSHITTHATIHADEVERWROTE : MonoBehaviour
     private void Awake()
     {
         print(STUPIDSHIT.eulerAngles);
-        SHIT = ((Transform[])FindObjectsOfType(typeof(Transform))).Where(x => x.localEulerAngles.x == STUPIDSHIT.eulerAngles.x).ToArray();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        print(STUPIDSHIT.eulerAngles);
+        SHIT = FindObjectsByType<Transform>(FindObjectsSortMode.None).Where(x => x.localEulerAngles.x == STUPIDSHIT.eulerAngles.x).ToArray();
     }
 }
