69c0a9064326e417b530edb7b85e756c20604eff
Foxy209 <79406460+Foxy209@users.noreply.github.com>
2025-07-26 03:00:01 +0500

    sceneTower

---
+++ Assets/Scripts/trigger_svin_tower.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class trigger_svin_tower : MonoBehaviour
+{
+    [SerializeField] private Animator svin;
+    [SerializeField] private GameObject _svA;
+    [SerializeField] private GameObject _svR;
+    [SerializeField] private GameObject _pl;
+    [SerializeField] private GameObject _spwP;
+    [SerializeField] private GameObject _spwS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FPSPlayer"))
+        {
+            _pl.SetActive(false);
+            _svA.SetActive(true);
+            svin.CrossFade("svinTOWER", 0, 0);
+            Invoke(nameof(before), 11);
+        }
+    }
+
+    private void before()
+    {
+        _svR.transform.position = _spwS.transform.position;
+        _pl.transform.position = _spwP.transform.position;
+        _pl.SetActive(true);
+        _svR.SetActive(true);
+        _svA.SetActive(false);
+    }
+}
