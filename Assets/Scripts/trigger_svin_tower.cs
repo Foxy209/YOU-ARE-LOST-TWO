132c08a9a19a5bbce6dd1fd48a8c22a35da65adb
Foxy209 <79406460+Foxy209@users.noreply.github.com>
2025-07-27 02:25:40 +0500

    houseTriggers

--- Assets/Scripts/trigger_svin_tower.cs
+++ Assets/Scripts/trigger_svin_tower.cs
@@ -7,9 +7,11 @@ public class trigger_svin_tower : MonoBehaviour
     [SerializeField] private GameObject _svA;
     [SerializeField] private GameObject _svR;
     [SerializeField] private GameObject _pl;
+    [SerializeField] private GameObject _pl_next;
     [SerializeField] private GameObject _spwP;
     [SerializeField] private GameObject _spwS;
-
+    [SerializeField] private AudioClip clip;
+    [SerializeField] private AudioSource sour_change;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("FPSPlayer"))
@@ -23,10 +25,13 @@ public class trigger_svin_tower : MonoBehaviour
 
     private void before()
     {
+        sour_change.clip = clip;
+        sour_change.Play();
         _svR.transform.position = _spwS.transform.position;
-        _pl.transform.position = _spwP.transform.position;
-        _pl.SetActive(true);
+        _pl_next.transform.position = _spwP.transform.position;
+        _pl_next.SetActive(true);
         _svR.SetActive(true);
         _svA.SetActive(false);
+        
     }
 }
